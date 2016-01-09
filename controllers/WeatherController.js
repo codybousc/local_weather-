d2da5df7a2e0d21cd737f89fc4a3fca8b65b863e
Cody Bouscaren <codybousc@gmail.com>
2016-01-09 13:37:29 -0500

    HTTP calls grabbing ip address and weather based off of city

--- controllers/WeatherController.js
+++ controllers/WeatherController.js
@@ -1,6 +1,32 @@
-angular.module("localWeather", []).controller('WeatherController', ['$scope',
-function($scope) {
+angular.module("localWeather", []).controller('WeatherController', ['$scope','$http',
+function($scope, $http) {
 
-$scope.words = "HELLO THERE"; 
+
+  $http({
+    method: 'GET',
+    url: 'http://ipinfo.io'
+  }).then(function successCallback(response) {
+    $scope.ipaddress = response;
+    $scope.city = response.data.city.replace(/\s/g, '');
+    //nested http requests, so that the openweathermap api request is only made once the $scope.city variable has been returned
+    //this is probably bad form, and i'm sure there's a 'correct' way to do this, but it works!
+      $http({
+          method: 'GET',
+          // url: 'http://api.openweathermap.org/data/2.5/weather?q='+ $scope.city + ',ng&units=imperial&appid=f800e45312042e21e1423e29a68bdd8c'
+          url: 'http://api.openweathermap.org/data/2.5/weather?q=NewYork,ng&units=imperial&appid=f800e45312042e21e1423e29a68bdd8c'
+        }).then(function successCallback(response) {
+          $scope.userWeather = response;
+          $scope.minTemp = response.data.main.temp_min;
+          $scope.maxTemp = response.data.main.temp_max; 
+
+          console.log(response);
+        }, function errorCallback(response) {
+          console.log(response);
+        });
+    console.log(response);
+  }, function errorCallback(response) {
+    console.log(response);
+  });
+
 
 }]);
